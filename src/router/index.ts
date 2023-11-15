5835d921d29835df30f70a5247f9554df8c1cd68
bgeukl <bgeukl2007bg@gmail.com>
2023-11-15 12:31:07 +0500

    Footer

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // import HomeView from "../views/HomeView.vue";
 import Main from "../components/Carousel/MainCarousel.vue";
 import Product from "../components/Carousel/ProdCarousel.vue";
+import Footer from "../components/footer/footer.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,7 @@ const router = createRouter({
       component: Main,
     },
     { path: "/prod", name: "Prod", component: Product },
+    { path: "/footer", name: "Footer", component: Footer },
   ],
 });
 
