01d7a95cd7fcdfb6dba4e2aead87b412bbc7556f
asilJPG <asilchik18@gmail.com>
2023-11-15 12:33:48 +0500

    main home page

---
+++ src/pages/Main/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Banner />
+    <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
+    <Product />
+    <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
+    <Product />
+    <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
+    <Product />
+  </div>
+</template>
+
+<script setup>
+import Banner from "../../components/Carousel/MainCarousel.vue";
+import Product from "../../components/Carousel/ProdCarousel.vue";
+</script>
+
+<style lang="scss" scoped></style>
