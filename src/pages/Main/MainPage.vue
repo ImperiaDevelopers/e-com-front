cfb5f8421f7d4ef9552a8b0150e1a36de54c96cc
Temur14 <rtemurbek77@gmail.com>
2023-11-16 10:29:44 +0500

    brand_category

--- src/pages/Main/MainPage.vue
+++ src/pages/Main/MainPage.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <Banner />
+    <Brand />
     <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
     <Product />
     <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
     <Product />
     <h1 class="text-[32px] color-[#000000] pl-[131px]">Most popular product</h1>
     <Product />
+    <Category />
   </div>
 </template>
 
 <script setup>
 import Banner from "../../components/Carousel/MainCarousel.vue";
+import Brand from "../../components/Brand/Brand.vue";
 import Product from "../../components/Carousel/ProdCarousel.vue";
+import Category from "../../components/Category/Category.vue";
 </script>
 
 <style lang="scss" scoped></style>
