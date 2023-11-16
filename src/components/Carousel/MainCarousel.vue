98dfa1d4d04b8ec827d1db5ce91d432c0e04477e
asilJPG <asilchik18@gmail.com>
2023-11-16 10:12:10 +0500

    main page style

--- src/components/Carousel/MainCarousel.vue
+++ src/components/Carousel/MainCarousel.vue
@@ -1,11 +1,8 @@
 <template>
   <el-carousel :interval="5000" arrow="always">
     <el-carousel-item v-for="(img, index) in imgs" :key="index">
-      <div
-        :style="{ background: img.bgColor }"
-        class="flex justify-between items-center"
-      >
-        <div class="flex flex-col items-start pl-[200px] gap-4">
+      <div :style="{ background: img.bgColor }" class="flex justify-between">
+        <div class="flex flex-col items-start pl-[200px] gap-4 pt-16">
           <h1 class="text-2xl">{{ img.text }}</h1>
           <p class="text-x">{{ img.desc }}</p>
           <button
@@ -28,20 +25,26 @@ const imgs = ref([
   {
     text: "Siz kutgan Xiaomi 12 Mi Laite     ",
     desc: "Orginallik va qulay narxni o'zida jamlagan  Xiaomi 12 Mi Laite  siz uchun eng yaxshi arziydigan takliflarimizdan biridir!ii",
-    content: "src/assets/images/image 1855.png",
+    content: "src/assets/images/product-imgs/image 1855.png",
     bgColor: "#F3F0F0",
   },
   {
-    text: "Siz kutgan Xiaomi 12 Mi Laite     ",
-    desc: "Orginallik va qulay narxni o'zida jamlagan  Xiaomi 12 Mi Laite  siz uchun eng yaxshi arziydigan takliflarimizdan biridir!ii",
-    content: "src/assets/images/Apple-iPhone-15-transformed.png",
+    text: "iPhone 15 Pro",
+    desc: "iPhone 15 Pro: Katta o‘nlayn muhit, kuchli kamera va tez ishlovchi kuchli texnologiyalar bilan yaratilgan.",
+    content: "src/assets/images/product-imgs/Apple-iPhone-15-transformed.png",
     bgColor: "#DBBE6E",
   },
   {
-    text: "Siz kutgan Xiaomi 12 Mi Laite     ",
-    desc: "Orginallik va qulay narxni o'zida jamlagan  Xiaomi 12 Mi Laite  siz uchun eng yaxshi arziydigan takliflarimizdan biridir!ii",
-    content: "src/assets/images/Remove-bg.ai_1700024011159.png",
-    bgColor: "#DBCB6E",
+    text: "Samsung S21 plus",
+    desc: "Galaxy S21 Plus: Yorqin ekran, kuchli kamera to'plami va tez ishlovchi hisoblanadi, dizayn va kuchli xususiyatlar bilan ajralgan.",
+    content: "src/assets/images/product-imgs/Remove-bg.ai_1700109929119.png",
+    bgColor: "#A7C9C4",
+  },
+  {
+    text: "Samsung Galaxy Z Fold4",
+    desc: "Galaxy Z Fold: Yuqori tekislikli jamlanadigan, keng ekranli, tez ishlovchi qurilmadir, moddiy dizayn.",
+    content: "src/assets/images/product-imgs/Remove-bg.ai_1700110541839.png",
+    bgColor: "#D9CBCA",
   },
 ]);
 </script>
--- src/components/Carousel/ProdCarousel.vue
+++ src/components/Carousel/ProdCarousel.vue
@@ -7,14 +7,15 @@
     class="mt-[20px]"
   >
     <Slide v-for="(img, index) in imgs" :key="index">
-      <div
-        class="carousel__item w-[273px] h-[330px] bg-[#EBEFF3] rounded"
-        style="{ width: img.width, height: img.height }"
-      >
+      <div class="carousel__item w-[273px] h-[330px] bg-[#EBEFF3] rounded">
         <div class="flex-col">
-          <img class="m-auto" :src="img.content" alt="Slide Image" />
+          <img
+            class="m-auto h-3/4 w-2/3"
+            :src="img.content"
+            alt="Slide Image"
+          />
           <h4 class="text-start mt-2">{{ img.text }}</h4>
-          <p class="text-2xl text-start pt-5">{{ img.price }}</p>
+          <p class="text-2xl text-start items-end pt-5">{{ img.price }}</p>
         </div>
       </div>
     </Slide>
@@ -33,51 +34,23 @@ const imgs = ref([
   {
     text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
     price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "50px",
-    height: "30px",
+    content: "src/assets/images/product-imgs/image-removebg-preview (37) 1.png",
   },
+  {
+    text: "AirPods Pro: Ergonomik dizayn, yuqori sifat, shumni o‘chirish va maslahatlar beradi.",
+    price: "6 999 999 usz ",
+    content: "src/assets/images/product-imgs/Remove-bg.ai_1700110810501.png",
+  },
+
   {
     text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
     price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
+    content: "src/assets/images/product-imgs/image-removebg-preview (37) 1.png",
   },
   {
-    text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+    text: "AirPods Pro: Ergonomik dizayn, yuqori sifat, shumni o‘chirish va maslahatlar beradi.",
     price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
-  },
-  {
-    text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
-  },
-  {
-    text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
-  },
-  {
-    text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
-  },
-  {
-    text: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 usz ",
-    content: "src/assets/images/image-removebg-preview (37) 1.png",
-    width: "30px",
-    height: "20px",
+    content: "src/assets/images/product-imgs/Remove-bg.ai_1700110810501.png",
   },
 ]);
 
