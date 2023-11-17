10d014c4bdec2c5352609cd7e10e6f2cad08959a
bgeukl <bgeukl2007bg@gmail.com>
2023-11-17 10:36:25 +0500

    payment

---
+++ src/components/Payments/payment.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="flex flex-wrap gap-5 m-5 w-[820px] justify-center">
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image 1831.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">Payme orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]"> 
+        <img class="flex m-10 " src="src/assets/payment_images/image 1832@2x (1).png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">Click orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image-removebg-preview (6) 1.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">Uzcard orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image-removebg-preview (7) 1.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">Humo orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image 1833.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">VISA orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image 1834.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">MasterCard orqali to’lov qilish</h1>
+    </div>
+   <div class="flex w-[400px] h-[120px] rounded-[6px] bg-[#EBEFF3] gap-[20px] items-center hover:bg-[#e5effa]">
+        <img class="flex m-10 " src="src/assets/payment_images/image-removebg-preview (8) 1.png" alt="">
+        <h1 class="flex ml-[-40px] font-semibold">Naqd orqali to’lov qilish</h1>
+    </div>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
