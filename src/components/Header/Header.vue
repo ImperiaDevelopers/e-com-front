b3821167c44e4ff7267b1cf665faff7bd0600fff
asilJPG <asilchik18@gmail.com>
2023-11-21 11:11:17 +0500

    basket w-1180px

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,129 +1,132 @@
 <template>
-  <div class="w-full flex flex-col  justify-center items-center">
-
-  <!-- First layer -->
-  <div
-    class="bg-[#EBEFF3] w-full h-[40px] flex pt-2 justify-center gap-[600px] items-center"
-  >
-    <div class="first flex gap-8">
-      <div class="location flex gap-3">
-        <i class="fa-solid fa-location-dot mt-1"></i>
-        <h3>Location</h3>
-      </div>
-      <div class="others flex gap-5">
-        <a href="#">About us</a>
-        <a href="#">Products</a>
-        <router-link to="feedback"> <a>Contacts</a></router-link>
-      </div>
-    </div>
-    <div class="second flex gap-8">
-      <h1 class="raqam text-[#545D6A]">+998(71) 123-45-67</h1>
-      <select
-        class="bg-[inherit] text-[#545D6A] outline-none"
-        name="lan"
-        id="lan"
-      >
-        <option class="" value="uzb">Uz</option>
-        <option value="en">En</option>
-        <option value="rus">Ru</option>
-      </select>
-    </div>
-  </div>
-
-  <!-- Second layer -->
-  <div class="bg bg-white h-[140px]  flex flex-col gap-5 items-centerm ">
-    <div class="row1 flex flex-row  justify-between items-center mt-4">
-      <h1
-        class="flex flex-row  items-center text-[#134E9B] font-['Roboto'] text-[36px] font-black"
-      >
-      
-        <img
-          src="src/assets/images/logo.png"
-          class="w-[48px]"
-          alt="src/assets/images/logo.png"
-        />
-        Ashyo
-      </h1>
-
-      <div class="flex gap-3">
-        <button
-          class="bg-[#134E9B] text-white  w-[130px] rounded-md font-['Roboto'] outline-none flex  items-center justify-around hover:bg-[#0c56b6]"
-          id="lan"
-        >
-        Kategoriya
-        <i class="fa-solid fa-chevron-down text-[10px]"></i>
-        </button>
-        <div>
-          <input
-            type="text"
-            placeholder="What are you looking for?"
-            class="ps-5 pe-5 p-[12.3px] color-[#EBEFF3] bg-[#EBEFF3] text-[13px] w-[518px] rounded-md outline-none"
-          />
-          <i
-            class="fa-solid fa-magnifying-glass bg-[#134E9B] text-white p-[14px] rounded-md cursor-pointer hover:bg-[#0c56b6]"
-            style="margin-left: -10px"
-          ></i>
+  <div class="w-full flex flex-col justify-center items-center">
+    <!-- First layer -->
+    <div
+      class="bg-[#EBEFF3] w-full h-[40px] flex pt-2 justify-center gap-[600px] items-center"
+    >
+      <div class="first flex gap-8">
+        <div class="location flex gap-3">
+          <i class="fa-solid fa-location-dot mt-1"></i>
+          <h3>Location</h3>
+        </div>
+        <div class="others flex gap-5">
+          <a href="#">About us</a>
+          <a href="#">Products</a>
+          <router-link to="feedback"> <a>Contacts</a></router-link>
         </div>
       </div>
+      <div class="second flex gap-8">
+        <h1 class="raqam text-[#545D6A]">+998(71) 123-45-67</h1>
+        <select
+          class="bg-[inherit] text-[#545D6A] outline-none"
+          name="lan"
+          id="lan"
+        >
+          <option class="" value="uzb">Uz</option>
+          <option value="en">En</option>
+          <option value="rus">Ru</option>
+        </select>
+      </div>
+    </div>
 
-      <div class="buttons flex gap-5 items-center">
-        <router-link to="/compare">
-          <i
-            class="fa-solid fa-scale-unbalanced-flip p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
+    <!-- Second layer -->
+    <div class="bg bg-white h-[140px] flex flex-col gap-5 items-centerm">
+      <div class="row1 flex flex-row justify-between items-center mt-4">
+        <a href="/">
+          <h1
+            class="flex flex-row items-center text-[#134E9B] font-['Roboto'] text-[36px] font-black"
           >
-          </i>
-          <p
-            class="count font['Roboto'] text-[10px] w-[20px] h-[20px] flex justify-center items-center bg-red-500 text-white rounded-full"
+            <img
+              src="src/assets/images/logo.png"
+              class="w-[48px]"
+              alt="src/assets/images/logo.png"
+            />
+            Ashyo
+          </h1></a
+        >
+
+        <div class="flex gap-3">
+          <button
+            class="bg-[#134E9B] text-white w-[130px] rounded-md font-['Roboto'] outline-none flex items-center justify-around hover:bg-[#0c56b6]"
+            id="lan"
           >
-            2
-          </p>
-        </router-link>
-        <router-link to="/like">
+            Kategoriya
+            <i class="fa-solid fa-chevron-down text-[10px]"></i>
+          </button>
           <div>
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              class="ps-5 pe-5 p-[12.3px] color-[#EBEFF3] bg-[#EBEFF3] text-[13px] w-[518px] rounded-md outline-none"
+            />
             <i
-              class="fa-regular fa-heart p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
+              class="fa-solid fa-magnifying-glass bg-[#134E9B] text-white p-[14px] rounded-md cursor-pointer hover:bg-[#0c56b6]"
+              style="margin-left: -10px"
             ></i>
-            <p
-              class="count font['Roboto'] text-[10px] w-[20px] h-[20px] flex justify-center items-center bg-red-500 text-white rounded-full"
-            >
-              11
-            </p>
           </div>
-        </router-link>
+        </div>
 
-        <router-link to="/basket">
-          <div>
+        <div class="buttons flex gap-5 items-center">
+          <router-link to="/compare">
             <i
-              class="fa-solid fa-cart-shopping p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
+              class="fa-solid fa-scale-unbalanced-flip p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
             >
             </i>
             <p
               class="count font['Roboto'] text-[10px] w-[20px] h-[20px] flex justify-center items-center bg-red-500 text-white rounded-full"
             >
-              7
+              2
             </p>
-          </div></router-link
-        >
+          </router-link>
+          <router-link to="/like">
+            <div>
+              <i
+                class="fa-regular fa-heart p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
+              ></i>
+              <p
+                class="count font['Roboto'] text-[10px] w-[20px] h-[20px] flex justify-center items-center bg-red-500 text-white rounded-full"
+              >
+                11
+              </p>
+            </div>
+          </router-link>
 
-        <i
-          class="fa-solid fa-user p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer hover:bg-[#d2d6da]"
-        ></i>
+          <router-link to="/basket">
+            <div>
+              <i
+                class="fa-solid fa-cart-shopping p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer mt-5 hover:bg-[#d2d6da]"
+              >
+              </i>
+              <p
+                class="count font['Roboto'] text-[10px] w-[20px] h-[20px] flex justify-center items-center bg-red-500 text-white rounded-full"
+              >
+                7
+              </p>
+            </div></router-link
+          >
+
+          <i
+            class="fa-solid fa-user p-3 bg-[#EBEFF3] rounded-md text-[#545D6A] cursor-pointer hover:bg-[#d2d6da]"
+          ></i>
+        </div>
+      </div>
+      <div class="w-[1180px] flex justify-between">
+        <a
+          href="#"
+          v-for="(item, index) in categories"
+          :key="index"
+          class="text-[#535c6a] font-['Roboto'] hover:text-[#2a2b2c] cat"
+          >{{ item }}</a
+        >
       </div>
     </div>
-    <div class="w-[1180px] flex justify-between">
-      <a href="#" v-for="(item,index) in categories" :key="index" class="text-[#535c6a] font-['Roboto'] hover:text-[#2a2b2c] cat"
-        >{{ item }}</a
-      >
-    </div>
   </div>
-  </div>
-
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from "vue";
 const categories = ref([
-  'Aksiyalar',
+  "Aksiyalar",
   "Televisions",
   "Computers & Laptops",
   "Cameras",
@@ -132,7 +135,7 @@ const categories = ref([
   "Accessories",
   "Kiryuvish mashinasi",
   "Konditsionerlar",
-])
+]);
 </script>
 
 <style lang="scss" scoped>
@@ -170,7 +173,7 @@ const categories = ref([
 }
 
 .cat::before {
-  content: '';
+  content: "";
   position: absolute;
   bottom: 0;
   left: 0;
--- src/pages/Feedback/FeedbackPage.vue
+++ src/pages/Feedback/FeedbackPage.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, magni.
-    Dolorum doloribus iste tempore sed consequatur eos esse magnam. Quo
-    explicabo consectetur officia labore officiis voluptatibus quos qui voluptas
-    possimus repudiandae dolorum aliquam distinctio voluptatem, architecto quasi
-    voluptates ipsa, soluta quia est nihil unde ex dolores nemo. Debitis, velit.
-    Libero necessitatibus veritatis dignissimos ea at, iure natus commodi, quos
-    vero maiores iusto explicabo veniam ipsum in! Voluptatibus delectus adipisci
-    dolores ea reiciendis pariatur illo accusamus numquam? Magni, obcaecati quam
-    inventore odio cum fuga harum. Minima, nam aperiam? Odit, numquam assumenda
-    libero, ducimus, quod amet minima aut enim ratione beatae adipisci?
+    <a href="{{ url }}">
+      <button>Tastiklash</button>
+    </a>
   </div>
 </template>
 
