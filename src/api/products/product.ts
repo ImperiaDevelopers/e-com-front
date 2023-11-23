99988f107b5d0e86408184a8c8507cf4a0d05763
ulugbek48 <ulugbekyuldashboyev@gmail.com>
2023-11-23 10:54:00 -0500

    performance

--- src/api/products/product.ts
+++ src/api/products/product.ts
@@ -7,13 +7,21 @@ interface ProductApi {
   updateProduct(payload: any, id: string): Promise<any>;
   getImage(product_id: any): Promise<any>;
   getFilter(params?: any): Promise<any>;
+  getProPerGroup(params?: any): Promise<any>;
+  getProPerGroupId(id: string): Promise<any>;
+  getPerfomance(params?: any): Promise<any>;
+  getPerfomanceID(id: string): Promise<any>;
+  getProInfo(params?: any): Promise<any>;
+  getProInfoID(id: string): Promise<any>;
 }
 
 const productApi: ProductApi = {
+  // ----------Pro Search --------- //
   getProductSearch(params) {
     const url = `product/search`;
     return axiosClient.post(url, params);
   },
+  // ----------Product------------- //
   getProduct(params) {
     const url = `product/all/q?page=${params?.page}&limit=${params?.limit}`;
     return axiosClient.get(url);
@@ -26,13 +34,42 @@ const productApi: ProductApi = {
     const url = `product/${id}`;
     return axiosClient.put(url, payload);
   },
+  // ----------- Pro Img ---------- //
   getImage(product_id) {
     const url = `image/products/${product_id}`;
     return axiosClient.get(url);
   },
+  // ---------- Pro Filter -------- //
   getFilter(params) {
     const url = `product/filter`;
     return axiosClient.post(url, params);
+  },
+  // --------- Pro Per Group ----- //
+  getProPerGroup(params) {
+    const url = `pro_perfomance_group`;
+    return axiosClient.get(url);
+  },
+  getProPerGroupId(id) {
+    const url = `pro_perfomance_group/${id}`;
+    return axiosClient.get(url);
+  },
+  // --------- Perfomance ------- //
+  getPerfomance(params) {
+    const url = `performance/all`;
+    return axiosClient.get(url);
+  },
+  getPerfomanceID(id) {
+    const url = `performance/${id}`;
+    return axiosClient.get(url);
+  },
+  // --------- Pro-Info -------- //
+  getProInfo(params) {
+    const url = `pro-info`;
+    return axiosClient.get(url);
+  },
+  getProInfoID(id) {
+    const url = `pro-info/${id}`;
+    return axiosClient.get(url);
   },
 };
 
--- src/stores/products/product.ts
+++ src/stores/products/product.ts
@@ -4,6 +4,9 @@ import productApi from "../../api/products/product";
 interface ProductState {
   products: any[];
   images: any[];
+  pro_per_group: any[];
+  performanse: any[];
+  per_info: any[];
 }
 
 export const useProductStore = defineStore({
@@ -11,6 +14,9 @@ export const useProductStore = defineStore({
   state: (): ProductState => ({
     products: [],
     images: [],
+    pro_per_group: [],
+    performanse: [],
+    per_info: [],
   }),
   actions: {
     async getProducts(params: any) {
@@ -55,6 +61,51 @@ export const useProductStore = defineStore({
     async getFilter(payload: any) {
       try {
         await productApi.getFilter(payload);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getProPerGroup(params: any) {
+      try {
+        const res = await productApi.getProPerGroup(params);
+        this.pro_per_group = res;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getProPerGroupId(groupId: string) {
+      try {
+        await productApi.getProPerGroupId(groupId);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getPerformance(params: any) {
+      try {
+        const res = await productApi.getPerfomance(params);
+        this.performanse = res;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getPerformanceId(perforID: string) {
+      try {
+        await productApi.getPerfomanceID(perforID);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getPerInfo(params: any) {
+      try {
+        const res = await productApi.getProInfo(params);
+        this.per_info = res;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getInfoId(infoId: any) {
+      try {
+        await productApi.getProInfoID(infoId);
       } catch (err) {
         console.error(err);
       }
