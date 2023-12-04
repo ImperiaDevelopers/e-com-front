7ad1de6dde8b4cf15f85c81c5c8c0847191f0988
asilJPG <asilchik18@gmail.com>
2023-12-04 11:08:27 +0500

    like

--- src/pages/User/UserPage.vue
+++ src/pages/User/UserPage.vue
@@ -50,7 +50,68 @@
           </el-collapse>
         </div>
         <div v-else>
-          <h1>1212</h1>
+          <div
+            class="w-[800px] p-5 flex gap-[50px] font-medium rounded-[10px] border bg-white border-[#12486B]/30 shadow-lg"
+          >
+            <div class="w-1/2 flex flex-col">
+              <vee-form
+                :validation-schema="schema"
+                :initial-values="props.data"
+                @submit="updateProfile"
+              >
+                <VInput
+                  type="text"
+                  name="first_name"
+                  label="First Name"
+                  placeholder="Enter your name"
+                  :disabled="disabled"
+                ></VInput>
+                <VInput
+                  type="text"
+                  name="last_name"
+                  label="Last Name"
+                  placeholder="Enter your surname"
+                  :disabled="disabled"
+                ></VInput>
+                <VInput
+                  type="text"
+                  name="phone"
+                  label="Phone"
+                  placeholder="(+998)-90-123-45-67"
+                  :mask="'(+998)-##-###-##-##'"
+                  :disabled="disabled"
+                ></VInput>
+                <div v-if="!disabled" class="flex gap-5">
+                  <VButton
+                    btn_type="danger"
+                    type="button"
+                    class="mt-5 w-full text-[18px]"
+                    @click="cancel"
+                  >
+                    Cancel
+                  </VButton>
+                  <VButton
+                    v-if="!disabled"
+                    btn_type="primary"
+                    type="submit"
+                    class="mt-5 w-full text-[18px]"
+                  >
+                    Save
+                  </VButton>
+                </div>
+                <VButton
+                  v-else
+                  btn_type="primary"
+                  :isLoading="false"
+                  type="button"
+                  class="mt-5 w-full text-[18px]"
+                  @click="disabled = false"
+                >
+                  Edit
+                </VButton>
+              </vee-form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -61,8 +122,26 @@
 <script setup lang="ts">
 import Header from "../../components/Header/Header.vue";
 import Footer from "../../components/Footer/Footer.vue";
+import VInput from "../../components/ui/VInput.vue";
+import VButton from "../../components/ui/VButton.vue";
 import { ref } from "vue";
 const changer = ref(true);
+
+const disabled = ref(true);
+
+const props = defineProps({
+  data: {
+    first_name: "",
+    last_name: "",
+    status: null,
+    phone: "",
+    image: "",
+    role: "",
+    start_date: "",
+    _id: "",
+  },
+});
+
 const orders = ref([
   {
     id: 1,
@@ -73,7 +152,7 @@ const orders = ref([
     data: "12.12.2012",
   },
   {
-    id: 1,
+    id: 2,
     title: "iphone 13",
     products: "pro, pro max",
     price: "123231",
@@ -81,6 +160,9 @@ const orders = ref([
     data: "12.12.2012",
   },
 ]);
+const cancel = () => {
+  disabled.value = true;
+};
 
 const activeNames = ref(["1"]);
 const handleChange = (val: string[]) => {
